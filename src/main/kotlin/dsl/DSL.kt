e78cdbd8855cf240f13521f2ccf8df7f617a5f0c
Saggiorato Enrico <e.saggiorato@reply.it>
2024-05-27 18:04:31 +0200

    add documentation

--- src/main/kotlin/dsl/DSL.kt
+++ src/main/kotlin/dsl/DSL.kt
@@ -8,10 +8,13 @@ import io.github.essay97.kastle.model.LinkBehavior
 annotation class KastleDsl
 
 /**
- * The entry point of the DSL
+ * The entry point of the DSL.
  *
- * @param initialRoomId the id of the first room that will be loaded in the game
- * @param init function that configures the room
+ * The game begins in the room specified by [initialRoomId]. Ensure that exactly one room
+ * has an ID matching this value.
+ * All game details, including metadata, rooms, characters, and the player,
+ * are defined through the [init] function.
+ *
  */
 @KastleDsl
 fun game(initialRoomId: String, init: GameScope.() -> Unit): GameConfiguration {
@@ -32,7 +35,13 @@ class GameScope(private val initialRoomId: String) {
     private var characters = mutableListOf<CharacterDto>()
     private var winningConditions: WinningConditionsDto? = null
 
+    /**
+     * The preface is a phrase or short text that, if defined, is printed at the very beginning of the game.
+     *
+     * It can be used to provide context about the game and its world.
+     */
     var preface: String? = null
+
 
 
     fun metadata(init: MetadataScope.() -> Unit) {
@@ -47,6 +56,13 @@ class GameScope(private val initialRoomId: String) {
         player = scope.build()
     }
 
+    /**
+     * Adds a room to the game that can be configured through the [init] function.
+     *
+     * The [roomId] is used to reference the room from other parts of the DSL when needed.
+     * It must start with <code>r-</code> and contain only lowercase letters, numbers and dashes.
+     * It must match the regex <code>^r-(?!-)(?:[a-z0-9]+-)*[a-z0-9]+$</code>.
+     */
     fun room(roomId: String, init: RoomScope.() -> Unit) {
         val scope = RoomScope(roomId)
         scope.init()
@@ -56,13 +72,16 @@ class GameScope(private val initialRoomId: String) {
         rooms += result.room
     }
 
+    /**
+     * Sets the winning conditions for the entire game.
+     */
     fun winIf(init: WinningConditionsScope.() -> Unit) {
         val scope = WinningConditionsScope()
         scope.init()
         winningConditions = scope.build()
     }
 
-    fun build(): GameConfiguration = GameConfiguration(
+    internal fun build(): GameConfiguration = GameConfiguration(
         initialRoomId = initialRoomId,
         metadata = metadata,
         player = player,
@@ -82,12 +101,25 @@ class GameScope(private val initialRoomId: String) {
 @KastleDsl
 class MetadataScope {
     var author: String? = null
+
+    /**
+     * The version of the game. It does not affect the game in any way, it's just documentation.
+     */
     var version: String? = null
     var published: LocalDate? = null
+
+    /**
+     * The versions of the engine compatible with this version of the game.
+     * It does not affect the game in any way, it's just documentation.
+     */
     var kastleVersions: List<String>? = null
+
+    /**
+     * The name of the game.
+     */
     var name: String? = null
 
-    fun build(): MetadataDto = MetadataDto(
+    internal fun build(): MetadataDto = MetadataDto(
         author = author,
         version = version,
         published = published,
@@ -101,10 +133,13 @@ class MetadataScope {
  */
 @KastleDsl
 class PlayerScope {
+    /**
+     * The name of the player. If not defined, defaults to "Player"
+     */
     var name = "Player"
     var description: String? = null
 
-    fun build(): PlayerDto = PlayerDto(
+    internal fun build(): PlayerDto = PlayerDto(
         name = name,
         description = description
     )
@@ -115,6 +150,9 @@ class PlayerScope {
  */
 @KastleDsl
 class RoomScope(private val roomId: String) {
+    /**
+     * The name of the room. If not defined, defaults to [roomId]
+     */
     var name = roomId
     var description: String? = null
 
@@ -125,31 +163,51 @@ class RoomScope(private val roomId: String) {
     private val items: MutableList<ItemDto> = mutableListOf()
     private val characters: MutableList<CharacterDto> = mutableListOf()
 
+    /**
+     * Links the room to another one referenced by [roomId] through the north door.
+     *
+     * If the [init] block is defined, you can use it to define the state and behavior of the door.
+     */
     fun north(roomId: String, init: DirectionScope.() -> Unit = {}) {
         val scope = DirectionScope(roomId)
         scope.init()
         north = scope.build()
     }
 
+    /**
+     * Links the room to another one referenced by [roomId] through the south door.
+     *
+     * If the [init] block is defined, you can use it to define the state and behavior of the door.
+     */
     fun south(roomId: String, init: DirectionScope.() -> Unit = {}) {
         val scope = DirectionScope(roomId)
         scope.init()
         south = scope.build()
     }
 
+    /**
+     * Links the room to another one referenced by [roomId] through the west door.
+     *
+     * If the [init] block is defined, you can use it to define the state and behavior of the door.
+     */
     fun west(roomId: String, init: DirectionScope.() -> Unit = {}) {
         val scope = DirectionScope(roomId)
         scope.init()
         west = scope.build()
     }
 
+    /**
+     * Links the room to another one referenced by [roomId] through the east door.
+     *
+     * If the [init] block is defined, you can use it to define the state and behavior of the door.
+     */
     fun east(roomId: String, init: DirectionScope.() -> Unit = {}) {
         val scope = DirectionScope(roomId)
         scope.init()
         east = scope.build()
     }
 
-    fun build(): BuildResult = BuildResult(
+    internal fun build(): BuildResult = BuildResult(
         room = RoomDto(
             name = name,
             id = roomId,
@@ -162,6 +220,13 @@ class RoomScope(private val roomId: String) {
         items = items
     )
 
+    /**
+     * Adds a character to the room that can be configured through the [init] function.
+     *
+     * The [characterId] is used to reference the character from other parts of the DSL when needed.
+     * It must start with <code>c-</code> and contain only lowercase letters, numbers and dashes.
+     * It must match the regex <code>^c-(?!-)(?:[a-z0-9]+-)*[a-z0-9]+$</code>.
+     */
     fun character(characterId: String, init: CharacterScope.() -> Unit) {
         val scope = CharacterScope(characterId)
         scope.init()
@@ -170,6 +235,13 @@ class RoomScope(private val roomId: String) {
         items += result.items
     }
 
+    /**
+     * Adds an item to the room that can be configured through the [init] function.
+     *
+     * The [itemId] is used to reference the room from other parts of the DSL when needed.
+     * It must start with <code>i-</code> and contain only lowercase letters, numbers and dashes.
+     * It must match the regex <code>^i-(?!-)(?:[a-z0-9]+-)*[a-z0-9]+$</code>.
+     */
     fun item(itemId: String, init: ItemScope.() -> Unit) {
         val scope = ItemScope(itemId)
         scope.init()
@@ -185,16 +257,26 @@ class RoomScope(private val roomId: String) {
  */
 @KastleDsl
 class DirectionScope(private val roomId: String) {
+    /**
+     * Indicates whether the door is open or closed. Defaults to open.
+     */
     var state = LinkState.OPEN
+    /**
+     * Indicates whether the state of the door can be changed by the player. By default it cannot change
+     */
     var behavior = LinkBehavior.CONSTANT
 
     private var triggers: List<String> = listOf()
 
+    /**
+     * Sets the items that can be used to change the state of the door, i.e., items that function as a "key".
+     * The items are referenced by their IDs.
+     */
     fun triggers(vararg itemIds: String) {
         triggers = itemIds.asList()
     }
 
-    fun build(): DirectionDto = DirectionDto(
+    internal fun build(): DirectionDto = DirectionDto(
         roomId = roomId,
         state = DirectionStateDto(state, behavior, triggers),
     )
@@ -205,6 +287,9 @@ class DirectionScope(private val roomId: String) {
  */
 @KastleDsl
 class CharacterScope(private val characterId: String) {
+    /**
+     * The name of the character. If not defined, defaults to [characterId]
+     */
     var name = characterId
     var description: String? = null
     private var matchers: List<String> = listOf()
@@ -212,6 +297,9 @@ class CharacterScope(private val characterId: String) {
     // This is needed to store the items that are created as dialogue rewards
     private var items: List<ItemDto> = mutableListOf()
 
+    /**
+     * Defines the strings that the player can use to reference this character.
+     */
     fun matchers(vararg words: String) {
         matchers = words.asList()
     }
@@ -224,7 +312,7 @@ class CharacterScope(private val characterId: String) {
         items = result.items
     }
 
-    fun build(): BuildResult = BuildResult(
+    internal fun build(): BuildResult = BuildResult(
         character = CharacterDto(
             id = characterId,
             name = name,
@@ -244,16 +332,26 @@ class CharacterScope(private val characterId: String) {
  */
 @KastleDsl
 class ItemScope(private val itemId: String) {
+    /**
+     * The name of the item. If not defined, defaults to [itemId]
+     */
     var name = itemId
     var description: String? = null
+
+    /**
+     * Indicates whether the item can be stored in the inventory by the player.
+     */
     var storable: Boolean = false
     private var matchers: List<String> = listOf()
 
+    /**
+     * Defines the strings that the player can use to reference this item.
+     */
     fun matchers(vararg words: String) {
         matchers = words.asList()
     }
 
-    fun build(): ItemDto = ItemDto(
+    internal fun build(): ItemDto = ItemDto(
         name = name,
         id = itemId,
         description = description,
@@ -274,6 +372,14 @@ class DialogueScope {
     // This is needed to store the items that are created as dialogue rewards
     private var items: MutableList<ItemDto> = mutableListOf()
 
+    /**
+     * Defines the first question of the dialogue, told by the NPC.
+     *
+     * The [questionId] is used to reference the room from other parts of the DSL when needed.
+     * It must start with <code>d-</code> and contain only lowercase letters, numbers and dashes.
+     * It must match the regex <code>^d-(?!-)(?:[a-z0-9]+-)*[a-z0-9]+$</code>.
+     * This function is mandatory for every dialogue definition.
+     */
     fun firstQuestion(questionId: String, init: QuestionScope.() -> Unit) {
         val scope =  QuestionScope(questionId)
         scope.init()
@@ -284,7 +390,14 @@ class DialogueScope {
             items += result.item
         }
     }
-
+    /**
+     * Defines every other question except the first of the dialogue (see [firstQuestion]).
+     *
+     * The [questionId] is used to reference the room from other parts of the DSL when needed.
+     * It must start with <code>d-</code> and contain only lowercase letters, numbers and dashes.
+     * It must match the regex <code>^d-(?!-)(?:[a-z0-9]+-)*[a-z0-9]+$</code>.
+     * This function is mandatory for every dialogue definition.
+     */
     fun question(questionId: String, init: QuestionScope.() -> Unit) {
         val scope =  QuestionScope(questionId)
         scope.init()
@@ -296,7 +409,7 @@ class DialogueScope {
 
     }
 
-    fun build(): BuildResult = BuildResult(
+    internal fun build(): BuildResult = BuildResult(
         dialogue = DialogueDto(
             firstQuestion = firstQuestionId,
             questions = questions
@@ -313,24 +426,39 @@ class DialogueScope {
  */
 @KastleDsl
 class QuestionScope(private val questionId: String) {
+    /**
+     * The actual question told by the character. Defaults to "Default question".
+     */
     var text = "Default question"
     private var reward: ItemDto? = null //Item id of the object that will be dropped
 
     private var answers: MutableList<AnswerDto> = mutableListOf()
 
+    /**
+     * Defines one possible answer for the question. To provide multiple options, invoke this function multiple times.
+     *
+     * - If at least one answer is defined, no [reward] can be defined
+     * - If no answer is defined, the question is considered a terminal node of the dialogue
+     */
     fun answer(init: AnswerScope.() -> Unit) {
         val scope = AnswerScope()
         scope.init()
         answers += scope.build()
     }
 
+    /**
+     * Defines the reward that the character awards the player at the end of the dialogue.
+     *
+     * If at least one answer is defined, this reward has no effect.
+     * The reward is never mandatory.
+     */
     fun reward(itemId: String, init: ItemScope.() -> Unit) {
         val scope = ItemScope(itemId)
         scope.init()
         reward = scope.build()
     }
 
-    fun build(): BuildResult = BuildResult(
+    internal fun build(): BuildResult = BuildResult(
         question = QuestionDto(
             id = questionId,
             question = text,
@@ -349,10 +477,18 @@ class QuestionScope(private val questionId: String) {
  */
 @KastleDsl
 class AnswerScope {
+    /**
+     * The actual answer told by the player if this answer option is chosen. Defaults to "Default answer".
+     */
     var text = "Default answer"
+
+    /**
+     * The next question in the dialogue if this answer option is chosen, referenced by its ID.
+     * Defaults to "d-default-question".
+     */
     var nextQuestion = "d-default-question"
 
-    fun build(): AnswerDto = AnswerDto(
+    internal fun build(): AnswerDto = AnswerDto(
         text = text,
         nextQuestion = nextQuestion
     )
@@ -361,13 +497,25 @@ class AnswerScope {
 /**
  * Works as the receiver for the [GameScope.winIf] function and provides the DSL to configure the conditions that
  * the player must meet in order to win the game.
+ *
+ * The conditions that can be met are:
+ * - the player owns a specific item
+ * - the player enters a specific room
+ *
+ * If both conditions are defined, fulfilling either one is sufficient to win the game.
  */
 @KastleDsl
 class WinningConditionsScope {
+    /**
+     * Indicates the item that the player must own in order to win the game, referenced by its ID.
+     */
     var playerOwns: String? = null
+    /**
+     * Indicates the room that the player must enter in order to win the game, referenced by its ID.
+     */
     var playerEnters: String? = null
 
-    fun build(): WinningConditionsDto = WinningConditionsDto(
+    internal fun build(): WinningConditionsDto = WinningConditionsDto(
         playerOwns = playerOwns,
         playerEnters = playerEnters
     )
--- src/main/kotlin/model/DungeonNode.kt
+++ src/main/kotlin/model/DungeonNode.kt
@@ -1,18 +1,31 @@
 package io.github.essay97.kastle.model
 
 enum class LinkBehavior {
+    /**
+     * The link can only transition from locked to open.
+     */
     OPENABLE {
         override val canOpen: Boolean = true
         override val canClose: Boolean = false
     },
+    /**
+     * The link can only transition from open to locked.
+     */
     LOCKABLE {
         override val canOpen: Boolean = false
         override val canClose: Boolean = true
     },
+    /**
+     * The link can transition both from locked to open and from open to locked.
+     */
     COMPLETE {
         override val canOpen: Boolean = true
         override val canClose: Boolean = true
     },
+
+    /**
+     * The link cannot change its state
+     */
     CONSTANT {
         override val canOpen: Boolean = false
         override val canClose: Boolean = false
